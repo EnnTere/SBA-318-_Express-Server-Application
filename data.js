1d37ecb2f2ca117c52137f1d7634f1ec11a3c158
Natalie <52940911+NatUXR@users.noreply.github.com>
2025-01-23 16:07:02 -0700

    Basic GET routes, mock database, and data retrival from database.

---
+++ data.js
@@ -0,0 +1,21 @@
+// Mock database
+
+const mockData = [
+  {
+    id: 1, 
+    name: "Item1",
+  },
+  {
+    id: 2, 
+    name: "Item2",
+  },
+  {
+    id: 3, 
+    name: "Item3",
+  },
+];
+
+
+
+
+module.exports = mockData;
--- index.js
+++ index.js
@@ -3,10 +3,67 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-  console.log("test");
-});
+// Import fake data
+const mockData = require("./data.js")
+
+
+//Middleware - parses JSON req & puts in req.body
+app.use(express.json());
+
+
+///// CRUD /////
+
+
+// READ - get all items
 
 app.get("/", (req, res) => {
-  res.send("index");
+  res.send("home");
+});
+
+app.get("/api", (req, res) => {
+  res.send("get");
+});
+
+app.get("/api/data", (req, res) => {
+  //res.send("data");
+  res.json(data);
+});
+
+app.get("/api/data/:name", (req, res) => {
+  //res.send("data");
+  const item = mockData.find((i) => i.name == req.params.name)
+  if (item) res.json(item);
+});
+
+
+// POST - creates new item
+app.post("/api", (req, res) => {
+  res.send("post");
+});
+
+
+// PATCH / PUT - Update an item
+app.put("/api", (req, res) => {
+  res.send("put");
+});
+
+app.patch("/api", (req, res) => {
+  res.send("patch");
+});
+
+
+
+// DELETE - delete an item
+app.post("/", (req, res) => {
+  res.send("delete");
+});
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server listening to ${port}`);
 });
